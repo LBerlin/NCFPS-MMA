48da5e992cd973fe88e9f82abd854b20754da948
LBerlin <lberl001@odu.edu>
2021-11-15 00:20:17 -0500

    Paclet file

--- NCFPS/PacletInfo.m
+++ NCFPS/PacletInfo.m
@@ -1,14 +1,14 @@
 (* Paclet Info File *)
 
-(* created 2020/05/08*)
+(* created 2021/11/15*)
 
 Paclet[
     Name -> "NCFPS",
-    Version -> "0.0.13",
+    Version -> "0.1.0",
     MathematicaVersion -> "6+",
     Extensions -> 
         {
-        	{"Kernel", Root -> ".", Context -> "NCFPS`"},
+            {"Kernel", Root -> ".", Context -> "NCFPS`"}, 
             {"Documentation", Language -> "English", MainPage -> "Guides/NoncommutativeFormalPowerSeriesNCFPS"}
         }
 ]
