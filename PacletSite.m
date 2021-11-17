869bb72f6c8290b71a39da931ff85b73a8252974
LBerlin <lberl001@odu.edu>
2021-11-17 01:38:48 -0500

    PacletSite formatting

---
+++ PacletSite.m
@@ -0,0 +1,14 @@
+PacletSite[
+	Paclet[
+		Name -> "NCFPS",
+    	Version -> "0.1.0",
+    	MathematicaVersion -> "6+",
+    	Extensions -> 
+        	{
+            	{"Kernel", Root -> ".", Context -> "NCFPS`"}, 
+            	{"Documentation", Language -> "English", 
+            		MainPage -> "Guides/NoncommutativeFormalPowerSeriesNCFPS"}
+        	}
+        Location -> "https://raw.githubusercontent.com/LBerlin/NCFPS-MMA/master"
+	]
+]
