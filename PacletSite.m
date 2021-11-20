2f7a404e9c7f6f2b873e62f77f6eb004417e312c
LBerlin <lberl001@odu.edu>
2021-11-19 21:54:32 -0500

    Branch names

--- PacletSite.m
+++ PacletSite.m
@@ -1,8 +1,7 @@
 (* Created with the Wolfram Language for Students - Personal Use Only : www.wolfram.com *)
-PacletSite[Paclet[Name -> "NCFPS", Version -> "0.1.1", 
+PacletSite[Paclet[Name -> "NCFPS", Version -> "0.1.2", 
   MathematicaVersion -> "6+", Extensions -> 
    {{"Kernel", Root -> ".", Context -> "NCFPS`"}, 
     {"Documentation", Language -> "English", 
      MainPage -> "Guides/NoncommutativeFormalPowerSeriesNCFPS"}}, 
-  Location -> 
-   "https://raw.githubusercontent.com/LBerlin/NCFPS-MMA/paclet-conversion"]]
+  Location -> "https://raw.githubusercontent.com/LBerlin/NCFPS-MMA/main"]]
