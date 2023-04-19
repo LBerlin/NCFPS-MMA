89e4f8551d165191eb82caded26b076d5a0ad337
LBerlin <5043966+LBerlin@users.noreply.github.com>
2023-04-19 13:20:02 -0400

    Move kernel files for build script

--- NCFPS/PacletInfo.m
+++ NCFPS/PacletInfo.m
@@ -4,12 +4,12 @@
 
 Paclet[
     Name -> "NCFPS",
-    Version -> "0.1.7",
+    Version -> "0.1.8",
     MathematicaVersion -> "6+",
     Extensions -> 
         {
-            {"Kernel", Root -> ".", Context -> "NCFPS`"}, 
-            {"Documentation", Language -> "English", MainPage -> "Guides/NoncommutativeFormalPowerSeriesNCFPS"}
+            {"Kernel", Root -> "Kernel", Context -> "NCFPS`"},
+            {"Documentation", Language -> "English", MainPage -> "Guides/NonCommutativeFormalPowerSeries"}
         }
 ]
 
